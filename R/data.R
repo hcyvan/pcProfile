f0811a794e7f0a545ecc610e3273f45711d6ab69
Navy Cheng <navych@126.com>
2019-01-16 23:13:54 +0800

    add document

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' The RNA-seq counts data from TCGA-PRAD project
+#'
+#' The counts data contain 551 samples with 499 tumor and 52 normal tissue.
+#'
+#' @format A data frame with 60483 rows and 551 samples.
+
+"prad.rna.count"
+
