9bb5718a96ed2c4b6859432df378998ff8846672
NavyCheng <navycheng2020@gmail.com>
2019-02-06 17:52:04 +0800

    fix jasper to jaspar

--- R/data.R
+++ R/data.R
@@ -1,11 +1,35 @@
 #' The RNA-seq counts data from TCGA-PRAD project
 #'
 #' The counts data contain 551 samples with 499 tumor and 52 normal tissue.
+#' The first 499 column is tumor tissue and the other 52 columns is nuormal tissue.
 #'
 #' @format A data frame with 60483 rows and 551 samples.
+#' @usage data("prad.rna.count")
 
 "prad.rna.count"
-"tf2gene.jasper"
+
+
+#' TF-targets dataset
+#'
+#' Get motif from Jaspar core database and scan the promoter region with fimo
+#'
+#' @format A data frame wth 'tf' and 'gene'
+#' @usage data("tf2gene.jaspar")
+"tf2gene.jaspar"
+
+#' TF-targets dataset
+#'
+#' Use MetaCluster data of GTRD
+#'
+#' @format A data frame wth 'tf' and 'gene'
+#' @usage data("tf2gene.gtrd")
 "tf2gene.trrust"
+
+#' TF-targets dataset
+#'
+#' Dataset from TRRUST
+#'
+#' @format A data frame wth 'tf' and 'gene'
+#' @usage data("tf2gene.trrust")
 "tf2gene.gtrd"
 
